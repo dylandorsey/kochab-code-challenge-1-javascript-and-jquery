20d1eb50f1e6ea4737b9c7fd381cebeeb1c46bf7
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-16 13:44:35 -0500

    functionality for delete button which deletes its parent div and contents thereof

--- client.js
+++ client.js
@@ -8,13 +8,14 @@ function onReady() {
     console.log('in jq');
     $('main').append('<button id="btn-generate">Generate</button>');
     generateBtn = $('#btn-generate');
-    generateBtn.on('click', clickHandler);
+    generateBtn.on('click', generateClickHandler);
 }
 
-function clickHandler() {
+function generateClickHandler() {
     appendDiv();
     generateButtonClickCounter++;
     console.log(generateButtonClickCounter);
+    console.log('generateClickHandler called')
 }
 
 function appendDiv() {
@@ -22,18 +23,31 @@ function appendDiv() {
     $('main').append('<div id="buttonClickCounter" class="generated-div"></div>');
     appendP();
     buttonClickCounterDiv = $('#buttonClickCounter');
-    buttonClickCounterDiv.append('<button id="btn-swap">Swap</button>');
-    buttonClickCounterDiv.append('<button id="btn-delete">Delete</button>');
+    buttonClickCounterDiv.append('<button class="btn-swap">Swap</button>');
+    buttonClickCounterDiv.append('<button class="btn-delete">Delete</button>');
+    $('.btn-swap').on('click', swapClickHandler);
+    $('.btn-delete').on('click', deleteClickHandler);
 }
 
 function appendP() {
-    $('#buttonClickCounter').empty();
     $('#buttonClickCounter').append(`
         <p>
         ${generateButtonClickCounter}
         </p>
     `);
-    console.log('button clicks: ' + buttonClickCounter);
 }
+
+function swapClickHandler(){
+    console.log('swapClickHandlerClicked');
+    console.log($('.btn-swap').parent());
+    $('.btn-swap').parent().remove();
+}
+
+function deleteClickHandler(){
+    console.log('deleteClickHandlerClicked');
+    $('.btn-delete').parent().remove();
+}
+
+// add click handler for swap button
 
 
