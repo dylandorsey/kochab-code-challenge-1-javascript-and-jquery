4d539f5f71355bf1ef2ca56b0485b5320f6a0b5f
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-16 14:34:28 -0500

    submission code

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 
 [X] In a CSS file, any <div> created using the "Generate" button should have a Red background-color.
 
-[ ] Clicking a "Swap" <button> should change its parent background-color from Red to Yellow.
+[x] Clicking a "Swap" <button> should change its parent background-color from Red to Yellow.
 
-[ ] Clicking the "Swap" <button> again should turn the color back to Red.
+[x] Clicking the "Swap" <button> again should turn the color back to Red.
 
-[ ] Clicking a "Delete" <button> should remove its parent <div> container (and all of its contents).
+[X] Clicking a "Delete" <button> should remove its parent <div> container (and all of its contents).
--- client.js
+++ client.js
@@ -3,6 +3,7 @@ console.log('in js');
 $(document).ready(onReady);
 
 generateButtonClickCounter = 0;
+let divID = 0;
 
 function onReady() {
     console.log('in jq');
@@ -12,40 +13,55 @@ function onReady() {
 }
 
 function generateClickHandler() {
-    appendDiv();
+    divID++;
     generateButtonClickCounter++;
-    console.log(generateButtonClickCounter);
-    console.log('generateClickHandler called')
+    appendDiv();
+    // console.log(generateButtonClickCounter);
+    // console.log('generateClickHandler called')
 }
 
 function appendDiv() {
     console.log('appendDiv function called');
-    $('main').append('<div id="buttonClickCounter" class="generated-div"></div>');
+    $('main').append('<div id="'+divID+'" class="generated-div"></div>');
     appendP();
-    buttonClickCounterDiv = $('#buttonClickCounter');
-    buttonClickCounterDiv.append('<button class="btn-swap">Swap</button>');
-    buttonClickCounterDiv.append('<button class="btn-delete">Delete</button>');
+    // buttonClickCounterDiv = $('#buttonClickCounter');
+    $('#'+divID).append('<button class="btn-swap">Swap</button>');
+    $('#'+divID).append('<button class="btn-delete">Delete</button>');
     $('.btn-swap').on('click', swapClickHandler);
     $('.btn-delete').on('click', deleteClickHandler);
+
+    function swapClickHandler(){
+
+        // this function is not working correctly. After it changes it's DOM appearance, it still holds its css background-color value.
+        // next I would figure out how to tie the color-swap logic to the DOM element's background-color value and not the css background-color valule.
+
+        console.log('swap click handler called');
+        console.log($(this).parent().css('background-color'));
+        if ($(this).parent().css('background-color') == 'rgb(255, 0, 0)') {
+            console.log('parent is red, change to yellow');
+            $(this).parent().css('background-color', 'rgb(255, 255, 0)');
+        }
+        else if ($(this).parent().css('background-color') == 'rgb(255, 255, 0)') {
+            console.log('parent is yellow, change to red');
+            $(this).parent().css('background-color', 'rgb(255, 0, 0)');
+        }
+    
+    }
 }
 
 function appendP() {
-    $('#buttonClickCounter').append(`
+    $('#'+divID).append(`
         <p>
         ${generateButtonClickCounter}
         </p>
     `);
 }
 
-function swapClickHandler(){
-    console.log('swapClickHandlerClicked');
-    console.log($('.btn-swap').parent());
-    $('.btn-swap').parent().remove();
-}
+
 
 function deleteClickHandler(){
     console.log('deleteClickHandlerClicked');
-    $('.btn-delete').parent().remove();
+    $(this).parent().remove();
 }
 
 // add click handler for swap button
