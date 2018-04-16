ae87792c5a57297924e90d80d066d507e24a927d
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-16 13:21:34 -0500

    append div upon clicking generate button

--- client.js
+++ client.js
@@ -4,7 +4,24 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('in jq');
-    $('main').append('<button id="btn-generage">Generate</button>');
+    $('main').append('<button id="btn-generate">Generate</button>');
+    generateBtn = $('#btn-generate');
+    generateBtn.on('click', clickHandler);
 }
+
+function clickHandler() {
+    appendDiv;
+    console.log('button clicked');
+    // $('#btn-generate').on('click', appendP);
+}
+
+function appendDiv() {
+    console.log('appendDiv function called');
+    $('main').append('<div id="buttonClickCounter" class="generated-div"></div>');
+}
+
+// function appendP() {
+
+// }
 
 
